a2e2a3ee436a97cd6ca704780c5fcb2a7ec47160
JL <adolfojohnlloyd5@gmail.com>
2023-10-26 22:46:25 +0800

    Updates

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 echo "Running composer"
-composer self-update 2.1.14
 composer global require hirak/prestissimo
 composer install --no-dev --working-dir=/var/www/html
 
@@ -13,14 +12,5 @@ php artisan route:cache
 echo "Running migrations..."
 php artisan migrate --force
 php artisan db::seed
-
-echo "Cleaning Cache..."
-npm cache clean --force
-
-echo "npm install"
-npm install
-
-echo "npm run build"
-npm run build
 
 echo "done deploying"
